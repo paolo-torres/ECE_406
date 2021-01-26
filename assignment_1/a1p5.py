6b6e1092130b837779e9521da1c42353e68674e1
Paolo Torres <paolo-torres@hotmail.com>
2021-01-25 22:51:14 -0500

    finish a1q5

--- assignment_1/a1p5.py
+++ assignment_1/a1p5.py
@@ -24,3 +24,31 @@ def expexp(x,y,z,p):
     We will test with inputs that match the spec only. I.e., all of
     x,y,z,p will be positive integers, and p will be a prime.
     """
+
+    """
+    Fermat's little theorem can be exploited to handle large values.
+    This theorem states that:
+        (a^p) is equivalent to (a mod p)
+    This is the same as:
+        (a^(p - 1)) is equivalent to (1 mod p)
+    Thus, modular exponentiation can be done with (p - 1) to get
+    (y^z mod (p - 1)), which is stored as b.
+    For each test, the b values are:
+        Test 1: b = 0
+        Test 2: b = 4
+        Test 3: b = 72
+        Test 4: b = 72
+    As shown, these values are much smaller to handle. Now, 
+    perform modular exponentiation again, this time with (p),
+    to get (x^(y^z) mod p), store as a, and return.
+    For each test, the a values are:
+        Test 1: a = 1
+        Test 2: a = 16
+        Test 3: a = 1
+        Test 4: a = 4
+    Each return value matches the expected values in the test,
+    therefore the algorithm is correct.
+    """
+    b = pow(y, z, p - 1)
+    a = pow(x, b, p)
+    return a
