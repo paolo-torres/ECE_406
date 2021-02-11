41da30ba7b313142c104f198ce9f526589ece595
Paolo Torres <paolo-torres@hotmail.com>
2021-02-10 20:52:00 -0500

    add assignment 2 files

---
+++ assignment_2/a2p4b.py
@@ -0,0 +1,23 @@
+"""
+ECE406, W'21, Assignment 2, Problem 3(b) 
+Skeleton solution file.
+"""
+
+"""
+You are not allowed to import anything. If we see any import
+statement, you earn an automatic 0.
+"""
+
+def nshortestpaths(G, a, b):
+    """
+    You need to implement this method.
+
+    You are certainly allowed to define any subroutines you want
+    above this method in this file.
+
+    We will test with inputs that match the spec only --- G will
+    be a non-empty graph, i.e., at least one vertex, and a, b will
+    be distinct valid vertices in G --- so that means G will have at
+    least 2 vertices. Of course within those constraints, we could test
+    for corner cases. E.g., a graph of no edges, e.g., G = [[],[],[]]
+    """
---
+++ assignment_2/tester-a2p4b.py
@@ -0,0 +1,84 @@
+#! /usr/bin/python3
+
+import sys
+import random
+from a2p4b import nshortestpaths
+
+"""
+Some tests.
+
+If you set this file to executable, and have the a1p4b.py file in
+the same folder, then you should be able to run this. You can and should
+of course try your own tests.
+"""
+
+def main():
+    print('Test 1:', end = ' ')
+    G = [[[1, 2], [3, 1]], [[0, 2], [3,1],[2,2]], [[1,2]], [[1, 1], [0, 1]]]
+    a = 0
+    b = 1
+    n = nshortestpaths(G, a, b)
+
+    if n == 2:
+        print('Passed')
+    else:
+        print('Failed')
+
+
+    print('Test 2:', end = ' ')
+    a = 0
+    b = 2
+    n = nshortestpaths(G, a, b)
+
+    if n == 2:
+        print('Passed')
+    else:
+        print('Failed')
+
+    print('Test 3:', end = ' ')
+    a = 0
+    b = 3
+    n = nshortestpaths(G, a, b)
+
+    if n == 1:
+        print('Passed')
+    else:
+        print('Failed')
+
+    print('Test 4:', end = ' ')
+    G.append([])
+    a = 0
+    b = 4
+    n = nshortestpaths(G, a, b)
+
+    if n == 0:
+        print('Passed')
+    else:
+        print('Failed')
+
+    print('Test 5:', end = ' ')
+    # example adapted from textbook
+    G = [[[1,4],[2,2]], [[0, 4],[2,1],[3,2],[4,3]], [[0,2],[1,1],[4,5],[3,4]], [[1,2],[2,4],[4,1]], [[1,3],[3,1],[2,5]]]
+    a = 0
+    b = 4
+    n = nshortestpaths(G, a, b)
+
+    if n == 2:
+        print('Passed')
+    else:
+        print('Failed')
+
+    print('Test 6:', end = ' ')
+    b = 3
+    n = nshortestpaths(G, a, b)
+
+    if n == 1:
+        print('Passed')
+    else:
+        print('Failed')
+
+
+
+
+if __name__ == '__main__':
+    main()
