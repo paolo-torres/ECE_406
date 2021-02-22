b8d49d29a67bdafaa5aeccd3187abcddcfc5d326
Paolo Torres <paolo-torres@hotmail.com>
2021-02-21 20:07:48 -0500

    clean up code for a2q4

--- assignment_2/a2p4b.py
+++ assignment_2/a2p4b.py
@@ -8,19 +8,6 @@ You are not allowed to import anything. If we see any import
 statement, you earn an automatic 0.
 """
 
-"""
-You need to implement this method.
-
-You are certainly allowed to define any subroutines you want
-above this method in this file.
-
-We will test with inputs that match the spec only --- G will
-be a non-empty graph, i.e., at least one vertex, and a, b will
-be distinct valid vertices in G --- so that means G will have at
-least 2 vertices. Of course within those constraints, we could test
-for corner cases. E.g., a graph of no edges, e.g., G = [[],[],[]]
-"""
-
 def getMinDistance(distances, visited, graphSize):
     minValue = float('inf')
     minIndex = 0
@@ -33,16 +20,17 @@ def getMinDistance(distances, visited, graphSize):
     return minIndex
 
 def updateDistance(G, u, v, distances, visited):
-    if (G[u][v]
-        and distances[u] != float('inf')
-        and distances[G[u][v][0]] > distances[u] + G[u][v][1]
-        and visited[G[u][v][0]] == False):
-        distances[G[u][v][0]] = distances[u] + G[u][v][1]
+    index = G[u][v][0]
+    value = G[u][v][1]
+
+    if (distances[u] != float('inf')
+        and distances[index] > distances[u] + value
+        and visited[index] == False):
+        distances[index] = distances[u] + value
 
 def getShortestPaths(G, distances, visited, graphSize):
     for i in range(graphSize):
         u = getMinDistance(distances, visited, graphSize)
-
         visited[u] = True
 
         for v in range(len(G[u])):
@@ -61,25 +49,33 @@ def traverse(G, src, cur, target, curDist, targetDist, curPath, numPaths):
         return
     
     for i in range(len(G[cur])):
-        if G[cur][i][0] == src or existsInPath(curPath, G[cur][i][0]):
+        index = G[cur][i][0]
+        value = G[cur][i][1]
+
+        if G[cur][i][0] == src or existsInPath(curPath, index):
             continue
 
-        curDist = curDist + G[cur][i][1]
-        curPath.append(G[cur][i][0])
+        curDist = curDist + value
+        curPath.append(index)
 
-        traverse(G, src, G[cur][i][0], target, curDist, targetDist, curPath, numPaths)
+        traverse(G, src, index, target, curDist, targetDist, curPath, numPaths)
         
-        curDist = curDist - G[cur][i][1]
+        curDist = curDist - value
         curPath.pop()
 
 def nshortestpaths(G, a, b):
-    # vertex: [other vertex, edge weight]
-    # a = 0, b = 1, c = 2, d = 3, e = 4
-    # a: [1, 4],[2, 2]
-    # b: [0, 4],[2, 1],[3, 2],[4, 3]
-    # c: [0, 2],[1, 1],[4, 5],[3, 4]
-    # d: [1, 2],[2, 4],[4, 1]
-    # e: [1, 3],[3, 1],[2, 5]
+    """
+    You need to implement this method.
+
+    You are certainly allowed to define any subroutines you want
+    above this method in this file.
+
+    We will test with inputs that match the spec only --- G will
+    be a non-empty graph, i.e., at least one vertex, and a, b will
+    be distinct valid vertices in G --- so that means G will have at
+    least 2 vertices. Of course within those constraints, we could test
+    for corner cases. E.g., a graph of no edges, e.g., G = [[],[],[]]
+    """
 
     graphSize = len(G)
 
