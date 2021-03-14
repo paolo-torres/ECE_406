e48c0c10845b3acd4c5f9a9c51572a7f037ab973
Paolo Torres <paolo-torres@hotmail.com>
2021-03-13 21:17:34 -0500

    clean up a3q1c code

--- assignment_3/a3p1c.py
+++ assignment_3/a3p1c.py
@@ -8,31 +8,7 @@ You are not allowed to import anything. If we see any import
 statement, you earn an automatic 0.
 """
 
-"""
-You need to implement this method.
-
-You are certainly allowed to define any subroutines you want
-above this method in this file.
-
-We will test with inputs that match the spec only --- a string
-str([[a,b], [c,d]]) is a valid key of c if and only if a move
-[a,b] to [c,d] is valid. src is a valid source square, i.e.,
-s[0] == 1. You should return a list, which is a path from the src
-square to one of the destination squares that is the cheapest
-from src to one of the destination squares.
-"""
-
-def robotpath(n, src, c):
-    xSrc = src[0] - 1
-    ySrc = src[1] - 1
-
-    dp = [[float('inf') for i in range(n)] for j in range(n)]
-    dp[xSrc][ySrc] = 1
-
-    numMoves = len(c)
-    moves = [[0 for i in range(numMoves)] for j in range(numMoves)]
-    costs = [0 for i in range(numMoves)]
-
+def parseMoves(c, moves, costs):
     i = 0
     for move in c:
         j = 0
@@ -46,8 +22,20 @@ def robotpath(n, src, c):
         i = i + 1
         j = 0
 
-    pos = []
+def buildDpArray(dp, pos, xCurr, yCurr, xNext, yNext, cost):
+    if (dp[xNext][yNext] > dp[xCurr][yCurr] + cost):
+        if (dp[xNext][yNext] != float('inf')):
+            for j in range(len(pos)):
+                if (pos[j][2] == xNext and pos[j][3] == yNext):
+                    if (pos[j][4] > cost):
+                        pos[j][0] = xCurr
+                        pos[j][1] = yCurr
+                        pos[j][4] = cost
+        else:
+            pos.append([xCurr, yCurr, xNext, yNext, cost])
+        dp[xNext][yNext] = dp[xCurr][yCurr] + cost
 
+def computePaths(dp, moves, costs, pos):
     for i in range(len(moves)):
         xCurr = moves[i][0]
         yCurr = moves[i][1]
@@ -59,23 +47,24 @@ def robotpath(n, src, c):
         if (dp[xCurr][yCurr] != float('inf')):
             print('Move:', xCurr, yCurr, '->', xNext, yNext, 'Cost:', cost)
 
-        if (dp[xNext][yNext] > dp[xCurr][yCurr] + cost):
-            if (dp[xNext][yNext] != float('inf')):
-                for j in range(len(pos)):
-                    if (pos[j][2] == xNext and pos[j][3] == yNext):
-                        if (pos[j][4] > cost):
-                            pos[j][0] = xCurr
-                            pos[j][1] = yCurr
-                            pos[j][4] = cost
-            else:
-                pos.append([xCurr, yCurr, xNext, yNext, cost])
-            dp[xNext][yNext] = dp[xCurr][yCurr] + cost
-    
-    for i in range(n):
-        print(dp[i])
-    
-    pos.reverse()
+        buildDpArray(dp, pos, xCurr, yCurr, xNext, yNext, cost)
 
+def getPath(n, pos, path, xStart, yStart, xEnd, yEnd, cost):
+    for i in range(n):
+        for j in pos:
+            if (xEnd == j[2]):
+                if (yEnd == j[3] and cost > j[4]):
+                    xStart = j[0]
+                    yStart = j[1]
+                    xEnd = j[2]
+                    yEnd = j[3]
+                    cost = j[4]
+        path.append([xEnd, yEnd])
+        xEnd = xStart
+        yEnd = yStart
+        cost = float('inf')
+
+def getShortestPath(n, pos, path):
     xStart = pos[0][0]
     yStart = pos[0][1]
     xEnd = pos[0][2]
@@ -90,22 +79,40 @@ def robotpath(n, src, c):
             yEnd = i[3]
             cost = i[4]
 
-    path = []
+    getPath(n, pos, path, xStart, yStart, xEnd, yEnd, cost)
+
+def robotpath(n, src, c):
+    """
+    You need to implement this method.
+
+    You are certainly allowed to define any subroutines you want
+    above this method in this file.
+
+    We will test with inputs that match the spec only --- a string
+    str([[a,b], [c,d]]) is a valid key of c if and only if a move
+    [a,b] to [c,d] is valid. src is a valid source square, i.e.,
+    s[0] == 1. You should return a list, which is a path from the src
+    square to one of the destination squares that is the cheapest
+    from src to one of the destination squares.
+    """
+
+    dp = [[float('inf') for i in range(n)] for j in range(n)]
+    dp[src[0] - 1][src[1] - 1] = 1
+
+    moves = [[0 for i in range(len(c))] for j in range(len(c))]
+    costs = [0 for i in range(len(c))]
+    
+    parseMoves(c, moves, costs)
+
+    pos = []
+    computePaths(dp, moves, costs, pos)
+    pos.reverse()
 
     for i in range(n):
-        for j in pos:
-            if (xEnd == j[2]):
-                if (yEnd == j[3] and cost > j[4]):
-                    xStart = j[0]
-                    yStart = j[1]
-                    xEnd = j[2]
-                    yEnd = j[3]
-                    cost = j[4]
-        path.append([xEnd, yEnd])
-        xEnd = xStart
-        yEnd = yStart
-        cost = float('inf')
-    
+        print(dp[i])
+
+    path = []
+    getShortestPath(n, pos, path)
     path.reverse()
 
     for i in range(len(path)):
